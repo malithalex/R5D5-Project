d99e99ccb43006fd48f9e915593c1a791a429ce9
Dulara-01 <124384611+Dulara-01@users.noreply.github.com>
2023-03-10 14:17:12 +0530

    Update playerpage.tsx

--- react/frontend-look/src/pages/playerpage/playerpage.tsx
+++ react/frontend-look/src/pages/playerpage/playerpage.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import styles from './playerpage.module.scss';
+import camilo from './sample/camilo.jpg';
 
 export interface PlayerpageProps {
     className?: string;
 }
 
 export const Playerpage: React.FC<PlayerpageProps> = ({ className = '' }) => (
-    <div className={className}>Playerpage
-    <br/>This page will showcase onece the user click on a player and his attributes and stats will display including a palyer value prediction button.
+    <div className={className}>
+        Playerpage<h2 className={styles.heading}>Two Players Side by Side</h2>
+        <img src={camilo} className={styles.topic} />
+        <select className={styles['drop-down']}>
+            <option>Lebron James</option>
+            <option>Stephen Curry</option>
+            <option>Kevin Durant</option>
+            <option>James Harden</option>
+            <option>Jeremy Lin</option>
+            <option>Chris Paul</option>
+            <option>Camilo Anthony</option>
+        </select>
     </div>
 );
