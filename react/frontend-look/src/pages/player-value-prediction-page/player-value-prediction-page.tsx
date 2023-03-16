70666b1229a2928ab5753509340dfade755adb47
Dulara-01 <124384611+Dulara-01@users.noreply.github.com>
2023-03-16 17:41:18 +0530

    Update player-value-prediction-page.tsx

--- react/frontend-look/src/pages/player-value-prediction-page/player-value-prediction-page.tsx
+++ react/frontend-look/src/pages/player-value-prediction-page/player-value-prediction-page.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import styles from './player-value-prediction-page.module.scss';
 
 export interface PlayerValuePredictionPageProps {
     className?: string;
 }
 
-export const PlayerValuePredictionPage: React.FC<PlayerValuePredictionPageProps> = ({ className = '' }) => (
-    <div className={className}>PlayerValuePredictionPage
-    <br/>"Once player value prediction button is clicked this page comes and showcase the predicted value including a compare button to compare players value with another player."
+export const PlayerValuePredictionPage: React.FC<PlayerValuePredictionPageProps> = ({
+    className = '',
+}) => (
+    <div className={className}>
+        PlayerValuePredictionPage
+        <br />
+        <img
+            src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg"
+            className={styles['plyer-img']}
+        />
+        <br />
+        TextParagraphs are the building blocks of papers. Many students define paragraphs in terms
+        of length: a paragraph is a group of at least five sentences, a paragraph is half a page
+        long, etc. In reality, though, the unity and coherence of ideas among sentences is what
+        constitutes a paragraph. A paragraph is defined as “a group of sentences or a single
+        sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not
+        determine whether a section in a paper is a paragraph. For instance, in some styles of
+        writing, particularly journalistic styles, a paragraph can be just one sentence long.
+        Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In
+        this handout, we will refer to this as the “controlling idea,” because it controls what
+        happens in the rest of the paragraph.
+        <br />
+        <button className={styles.button_cal_value}>
+            Calculate Player Value
+            <br />
+        </button>
     </div>
 );
